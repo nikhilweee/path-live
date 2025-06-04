1b85de75e7edc2273cd7be1db4b4af6db5f5e2ee
Nikhil Verma <nikhilweee@gmail.com>
2025-06-04 02:01:29 -0400

    remove flash animation

--- lib/widgets/message_card.dart
+++ lib/widgets/message_card.dart
@@ -15,30 +15,13 @@ class MessageCard extends StatefulWidget {
   State<MessageCard> createState() => _MessageCardState();
 }
 
-class _MessageCardState extends State<MessageCard>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<Color?> _animation;
+class _MessageCardState extends State<MessageCard> {
   bool _isHighlighted = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 500),
-      vsync: this,
-      value: 1.0,
-    );
-  }
 
   @override
   void didUpdateWidget(MessageCard oldWidget) {
     super.didUpdateWidget(oldWidget);
-
     if (oldWidget.message.lastUpdated != widget.message.lastUpdated) {
-      _animationController.reset();
-      _animationController.forward();
-
       // Reset highlight if this is likely a new train
       int oldSeconds = int.parse(oldWidget.message.secondsToArrival);
       int newSeconds = int.parse(widget.message.secondsToArrival);
@@ -49,60 +32,36 @@ class _MessageCardState extends State<MessageCard>
     }
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _animation = ColorTween(
-      begin: Theme.of(context).colorScheme.primary.withValues(alpha: 0.1),
-      end: Theme.of(context).cardColor,
-    ).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeOutQuart,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
   void _toggleHighlight() => setState(() => _isHighlighted = !_isHighlighted);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _toggleHighlight,
-      child: AnimatedBuilder(
-        animation: _animation,
-        builder: (_, child) => Card(
-          color: _isHighlighted
-              ? Theme.of(context).colorScheme.primaryContainer
-              : _animation.value,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: child,
-          ),
-        ),
-        child: Row(
-          children: [
-            ColorCircleWidget(colors: widget.message.lineColor),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(widget.message.headSign),
-                  RealTimeDisplay(
-                    secondsToArrival: widget.message.secondsToArrival,
-                    lastUpdated: widget.message.lastUpdated,
-                    arrivalTimeMessage: widget.message.arrivalTimeMessage,
-                  ),
-                ],
+      child: Card(
+        color: _isHighlighted
+            ? Theme.of(context).colorScheme.primaryContainer
+            : null,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              ColorCircleWidget(colors: widget.message.lineColor),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(widget.message.headSign),
+                    RealTimeDisplay(
+                      secondsToArrival: widget.message.secondsToArrival,
+                      lastUpdated: widget.message.lastUpdated,
+                      arrivalTimeMessage: widget.message.arrivalTimeMessage,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
